13c2950aab5fffb3ce9813ed900dc3afec2c5b2d
VeckovN <novaknolewow@gmail.com>
2024-12-29 18:35:15 +0100

    Add init helpers and github action

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//we need to import all exported interfaces 
+//interfaces that we want to use outside of library
+
+export {
+    IAuth
+} from './interfaces/authInterface';
+// } from './interfaces/auth.interface.ts';
---
+++ src/interfaces/authInterface.ts
@@ -0,0 +1,6 @@
+
+export interface IAuth {
+    id:number,
+    username:string,
+    email:string
+}
