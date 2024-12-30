d622c9110e2d85d40b3e94837b0d63ddae757d10
VeckovN <novaknolewow@gmail.com>
2024-12-30 13:44:08 +0100

    Remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-//we need to import all exported interfaces 
-//interfaces that we want to use outside of library
-
 export {
     IAuth
 } from './authInterface';
-// } from './interfaces/auth.interface.ts';
