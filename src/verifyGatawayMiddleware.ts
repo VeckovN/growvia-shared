48d24503462b38090b2b30ec66428e229512a47d
VeckovN <novaknolewow@gmail.com>
2025-01-24 20:40:11 +0100

    Add gateway verification middleware

---
+++ src/verifyGatawayMiddleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface payloadGatewayTokenRequest {
+    id: string, //Service name => etc. auth, user, order, payment
+    iat: number
+}
+
+enum serviceNames {
+    'auth', 
+    'user',
+    'order',
+    'product'
+}
+
+//middleware pased on every request (in each microservices)
+export function verifyGateway(req:Request, res:Response, next:NextFunction):void {
+    //check does JWT token exist in request header
+    const token = req.headers?.gatewayToken as string | undefined;
+    if(!token){
+        throw new Error("Token doesn't exist in request. Invalid request")
+    }
+
+    try{
+        //GATEWAY_JTW_TOKEN = 294bc0dd5baab813c5f762c3a8bba157 
+        //verify JWT token with "GATEWAY_JTW_TOKEN" key
+        const verifiedTokenPayload: payloadGatewayTokenRequest = verify(token ,'294bc0dd5baab813c5f762c3a8bba157') as payloadGatewayTokenRequest;
+        //check does token.id -> is some name of services ()
+        if(!Object.values(serviceNames).includes(verifiedTokenPayload.id)){
+            throw new Error("Invalid service name(ID) of the token");
+        }
+    }
+    catch(error){
+        throw new Error("Token Verification goes wrong")
+    }
+
+    next();
+}
