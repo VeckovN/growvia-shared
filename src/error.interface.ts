8f55e62ea298d012a33dcd29f6ef5c6e5f9c0b8b
VeckovN <novaknolewow@gmail.com>
2025-01-24 20:45:26 +0100

    Add error interface

---
+++ src/error.interface.ts
@@ -0,0 +1,20 @@
+// interface ErrorInterface{
+//     message: string;
+//     statusCode: number;
+//     status: string;
+// }
+export interface ErrorCustomResponseInterface {
+    message: string;
+    statusCode: number;
+    // errorObject(): ErrorInterface;
+}
+
+
+// Error{
+//     name: string;
+//     message: string;
+//     stack?: string;
+// }
+// class CustomError extends Error{
+
+// }   
