0a2ca973fc50bdb8a460bd40755a37ddee2c9933
VeckovN <novaknolewow@gmail.com>
2025-02-04 12:13:20 +0100

    Add helper function for checking email format

---
+++ src/helper.ts
@@ -0,0 +1,6 @@
+export function isEmailValid(email:string):boolean {
+    // do some checkes (maybe use third party library)
+    const regexExp =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+    return regexExp.test(email);
+}
