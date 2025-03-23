cebd1965ebdc80af8a3b02dcde45da804eb3aeb7
VeckovN <novaknolewow@gmail.com>
2025-03-23 21:33:34 +0100

    feat: change some props of NotificationInterface

--- src/notification.interface.ts
+++ src/notification.interface.ts
@@ -1,15 +1,16 @@
 export interface NotificationInterface {
     id?: string; //id type of Notification Service DB
-    orderID: string;
     senderID: string; //of can be get throught session -> currentUser.id
     senderUsername: string;
     senderEmail?: string;
     receiverID: string; 
     receiverUsername: string;
     receiverEmail?: string;
+    type: 'Order' | 'Authentication' | 'General';
     message: string;
     isRead: boolean;
-    type?: string;
+    orderID?: string;
     bothUsers?: boolean;
     createdAt?: Date;
 }
+
